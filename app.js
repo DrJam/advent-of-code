fad8f8c0d3dd4a8621e64052449d00ce9289e667
DrJam <dan@fordsville.co.uk>
2022-12-13 12:41:14 +0000

    day 13

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import day1 from "./day1/day1.js";
 import day10 from "./day10/day10.js";
 import day11 from "./day11/day11.js";
 import day12 from "./day12/day12.js";
+import day13 from "./day13/day13.js";
 import day2 from "./day2/day2.js";
 import day3 from "./day3/day3.js";
 import day4 from "./day4/day4.js";
@@ -23,7 +24,8 @@ function run() {
     // day9.execute();
     // day10.execute();
     // day11.execute();
-    day12.execute();
+    // day12.execute();
+    day13.execute();
     // dayX.execute();
 }
 
