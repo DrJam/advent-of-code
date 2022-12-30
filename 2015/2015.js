4029a87e8029d09be506cb86262b25c309467f9a
DrJam <dan@fordsville.co.uk>
2022-12-30 09:03:11 +0000

    day 4

--- 2015/2015.js
+++ 2015/2015.js
@@ -1,11 +1,13 @@
 import day1 from "./day-1/day-1.js";
 import day2 from "./day-2/day-2.js";
 import day3 from "./day-3/day-3.js";
+import day4 from "./day-4/day-4.js";
 
 function run() {
     // day1.execute()
     // day2.execute()
-    day3.execute()
+    // day3.execute()
+    day4.execute()
 }
 
 export { run }
--- common/common.js
+++ common/common.js
@@ -2,3 +2,5 @@ export const newline = '\r\n'
 export const doubleNewline = newline + newline
 export const coord = ([x, y]) => `${x},${y}`
 export const coord3 = ([x, y, z]) => `${x},${y},${z}`
+
+export { md5 } from './md5.js'
