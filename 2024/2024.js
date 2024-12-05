08cb1c274f84d24c3cd79fdc7fc0e103ca5fecef
Dan Ford <dan@fordsville.co.uk>
2024-12-05 09:17:06 +0000

    day 5 setup

--- 2024/2024.js
+++ 2024/2024.js
@@ -2,9 +2,10 @@ import day1 from "./day1/day1.js";
 import day2 from "./day2/day2.js";
 import day3 from "./day3/run.js";
 import day4 from "./day4/run.js";
+import day5 from "./day5/run.js";
 
 function run() {
-    day4.execute()
+    day5.execute()
 }
 
 export { run }
